13ec55518d9f4a9cd4e53df94ac230e67518b366
Patyczakus <patryk.topolski0707@gmail.com>
2021-12-22 13:50:02 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
-// Set the configuration for your app
-// TODO: Replace with your project's config object
-var config = {
-    apiKey: "AIzaSyBqt4RNoAbBRqDwmxJHJz79k62m6ZnZBmY",
-    authDomain: "chat-e8253.firebaseapp.com",
-    // For databases not in the us-central1 location, databaseURL will be of the
-    // form https://[databaseName].[region].firebasedatabase.app.
-    // For example, https://your-database-123.europe-west1.firebasedatabase.app
-    databaseURL: "https://databaseName.firebaseio.com",
-    storageBucket: "chat-e8253.appspot.com"
-};
-firebase.initializeApp(config);
-
-// Get a reference to the database service
-var database = firebase.database();
+window.onload = () => {
+    var config = {
+        apiKey: "AIzaSyBqt4RNoAbBRqDwmxJHJz79k62m6ZnZBmY",
+        authDomain: "chat-e8253.firebaseapp.com",
+        databaseURL: "https://databaseName.firebaseio.com",
+        storageBucket: "chat-e8253.appspot.com"
+    };
+    
+    const app = initializeApp(config);
+    console.log(getDatabase(app))
+}
